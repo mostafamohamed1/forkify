e0af9f679eedff6f328997f166e14f3805c4a4a7
Krypton-EG <mostafa.mohamed2700@gmail.com>
2021-03-07 17:28:17 +0200

    'initial'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '39c5207d-33c7-4c00-8128-d8429a2c3188';
+export const MODAL_CLOSE_SEC = 2.5;
